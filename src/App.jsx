304b7ddc3217e9dccd6022108c1b3ac989ddf597
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-10-03 10:34:50 +0300

    Test pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { Routes, Route, NavLink } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+export const App = () => {
+  return (
+    <div>
+      <Home />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+      
+      </Routes> */}
+      React homework template
+    </div>
+  );
+};
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Trending films</h1>
+      <ul>
+        <li>Name</li>
+        <li>Name</li>
+        <li>Name</li>
+      </ul>
+    </>
+  );
+}
